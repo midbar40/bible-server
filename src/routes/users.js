d6d8603c338b5e69f4b20f23e83c9cb694ce968c
seunghyun <inyo0506@gmail.com>
2023-11-30 14:50:22 +0900

    체이닝메서드위치변경

--- src/routes/users.js
+++ src/routes/users.js
@@ -11,7 +11,36 @@ const { Types : {ObjectId} } = mongoose
 const router = express.Router()
 
 
-router.post('/register', expressAsyncHandler(async(req, res, next)=>{
+router.post('/register', 
+[
+    body('name')
+    .exists()
+    .withMessage('이름을 입력하세요')
+    .isString()
+    .withMessage('이름은 문자만 입력가능합니다')
+    .bail(),
+body('email')
+    .exists()
+    .withMessage('이메일을 입력하세요')
+    .isEmail()
+    .withMessage('이메일 형식이 아닙니다')
+    .bail(),
+body('userId')
+    .exists()
+    .withMessage('아이디를 입력하세요')  
+    .isString()
+    .withMessage('아이디는 문자만 입력가능합니다')
+    .bail(),
+body('password')
+    .exists()
+    .withMessage('비밀번호를 입력하세요')
+    .isString()
+    .withMessage('비밀번호는 문자만 입력가능합니다')
+    .bail()
+]
+,expressAsyncHandler(async(req, res, next)=>{
+
+    
     const errors = validationResult(req)
     if(!errors.isEmpty()){
         console.log(errors.array())
@@ -21,32 +50,6 @@ router.post('/register', expressAsyncHandler(async(req, res, next)=>{
             error: errors.array()
         })
     }else{
-
-  body('name')
-      .exists()
-      .withMessage('이름을 입력하세요')
-      .isString()
-      .withMessage('이름은 문자만 입력가능합니다')
-      .bail(),
-  body('email')
-      .exists()
-      .withMessage('이메일을 입력하세요')
-      .isEmail()
-      .withMessage('이메일 형식이 아닙니다')
-      .bail(),
-  body('userId')
-      .exists()
-      .withMessage('아이디를 입력하세요')  
-      .isString()
-      .withMessage('아이디는 문자만 입력가능합니다')
-      .bail(),
-  body('password')
-      .exists()
-      .withMessage('비밀번호를 입력하세요')
-      .isString()
-      .withMessage('비밀번호는 문자만 입력가능합니다')
-      .bail()
-  
         const user = new User({
             name: req.body.name,
             email: req.body.email,
