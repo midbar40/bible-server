26214691933f12540f4de245aa6f1ee07a07d915
seunghyun <inyo0506@gmail.com>
2023-11-30 17:17:49 +0900

    validator 외부로 빼기

--- validator.js
+++ validator.js
@@ -2,25 +2,25 @@ const { body } = require('express-validator');
 
 const validate = [
         body('name')
-            .exists()
+            .isEmpty()
             .withMessage('이름을 입력하세요')
             .isString()
             .withMessage('이름은 문자만 입력가능합니다')
             .bail(),
         body('email')
-            .exists()
+            .isEmpty()
             .withMessage('이메일을 입력하세요')
             .isEmail()
             .withMessage('이메일 형식이 아닙니다')
             .bail(),
         body('userId')
-            .exists()
+            .isEmpty()
             .withMessage('아이디를 입력하세요')  
             .isString()
             .withMessage('아이디는 문자만 입력가능합니다')
             .bail(),
         body('password')
-            .exists()
+            .isEmpty()
             .withMessage('비밀번호를 입력하세요')
             .isString()
             .withMessage('비밀번호는 문자만 입력가능합니다')
