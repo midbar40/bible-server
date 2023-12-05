2e1949e8cccf9e328d8b33e3be07631c3b1adf0b
seunghyun <inyo0506@gmail.com>
2023-12-05 11:10:04 +0900

    버킷리스트 저장로직 수정

--- src/routes/prayBucketlist.js
+++ src/routes/prayBucketlist.js
@@ -1,29 +1,49 @@
 const express = require('express')
 const PrayBucketlist = require('../models/PrayBucketlist')
+const User = require('../models/User')
 const expressAsyncHandler = require('express-async-handler')
 
 const router = express.Router()
 
 // 기도버킷리스트 저장
 router.post('/', expressAsyncHandler(async(req, res, next)=> {
-    const prayBucketlist = new PrayBucketlist({
-        number: req.body.number,
-        detail: req.body.detail,
-        author: req.body.author,
-        createdAt: Date.now(),
-})
-    const newPrayBucketlist = await prayBucketlist.save()
-    if(!newPrayBucketlist){
-        res.status(401).json({ code:401, message: '기도 버킷리스트 저장 실패'})
-    }else{
-        const { number, detail, author, createdAt } = newPrayBucketlist
+    User.findOne({email: req.body.email})
+    .then(user =>{
+        if(user){
+            const prayBucketlist = new PrayBucketlist({
+                number: req.body.number,
+                detail: req.body.detail,
+                author: user._id
+            })
+            return prayBucketlist.save()
+        } else{
+            res.json({
+                code: 400,
+                message: '유저가 존재하지 않습니다.'
+            })
+        }
+    })
+    .then(result =>{
+        console.log('기도버킷리스트 저장 성공', result)
         res.json({
-            code:200,
-            message: '기도 버킷리스트 저장 성공',
-            number, detail, author, createdAt
+            code: 200,
+            message: '기도버킷리스트 저장 성공',
+            result
         })
-    }
+    })
+    .catch(err =>{
+        res.json({
+            code: 500,
+            message: '기도버킷리스트 저장 실패',
+            err
+        })
+    })
+
+
 }))
+   
+
+
 
 
 router.get('/', expressAsyncHandler(async(req, res, next)=>{
