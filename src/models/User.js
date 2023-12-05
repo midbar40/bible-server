d33f9bd0209138ca9d9dcfbba72f4f7718676109
seunghyun <inyo0506@gmail.com>
2023-12-05 10:27:31 +0900

    기도버킷리스트 서버

--- src/models/User.js
+++ src/models/User.js
@@ -29,6 +29,10 @@ const userSchema = new Schema({
         type: Date,
         default:Date.now,
     },
+    prayBucketlist:[{
+        type: ObjectId,
+        ref: 'PrayBucketlist',
+    }]
 })
 
 const User = mongoose.model('User', userSchema, 'users')
