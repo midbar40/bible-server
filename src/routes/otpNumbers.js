b4d2338db6bee95bb3759ca573e0dfc2a5a35e32
seunghyun <inyo0506@gmail.com>
2024-02-16 17:39:59 +0900

    resend Route 필요없어서 삭제

--- src/routes/otpNumbers.js
+++ src/routes/otpNumbers.js
@@ -13,85 +13,6 @@ let countNum = 1
 
 // otp 생성
 router.post('/generateOtp', expressAsyncHandler(async (req, res, next) => {
-    console.log('otp 생성 리퀘바디', req.body)
-
-    try {
-        const user = await User.findOne({ mobile: req.body.mobile, name: req.body.name  })
-
-        if (!user) {
-            res.json({
-                code: 400,
-                message: '등록된 사용자가 없습니다.'
-            })
-        } else {
-            const code = Math.floor(100000 + Math.random() * 900000).toString();
-                     
-            const otpNumber = new OtpNumber({
-                name: req.body.name,
-                mobile: req.body.mobile,
-                code: code,
-                count: countNum
-            })
-        if (otpNumber && otpNumber.count >= 4) {
-            res.json({
-                code: 400,
-                message: '인증 횟수 초과, 잠시 후 다시 시도해주세요.'
-            })
-            // 5분 뒤 count 초기화
-            setTimeout(() => {
-                countNum = 1
-            }, 300000)
-        }
-        if (otpNumber && otpNumber.count <= 3 && user.authCount <= 9) { 
-            const convertedPhoneNumber = `+82${req.body.mobile.slice(1)}`;
-            client.messages
-                .create({
-                    body: `인증번호는 ${code} 입니다.`,
-                    from: process.env.TWILIO_SENDER_PHONE,
-                    to: `${convertedPhoneNumber}`
-                })
-                .then(message => console.log(message.sid));
-            user.authCount = user.authCount + 1
-            countNum++    
-            console.log('유저정보',user)
-            
-            await otpNumber.save()
-            await user.save()
-
-            return res.json({
-                code: 200,
-                message: 'otp 생성 성공',
-                result: otpNumber
-            })
-        }
-        if (user.authCount >= 10) {
-            res.json({
-                code: 400,
-                message: '인증 횟수 초과, 3시간 후 다시 시도해주세요.'
-            })
-            // 3시간 후 authCount 초기화
-            setTimeout(() => {
-                console.log('3시간 후 authCount 초기화')
-                user.authCount = 0
-                user.save()
-            }, 60*60*3)
-        }
-        console.log('otpNumber :', otpNumber)
-  
-        }
-    }
-    catch (err) {
-        console.log('otp 생성 에러 :', err)
-        res.json({
-            code: 500,
-            message: 'otp 생성 실패',
-            err
-        })
-    }
-}))
-
-// otp 재전송
-router.post('/resendOtp', expressAsyncHandler(async (req, res, next) => {
     try {
         const user = await User.findOne({ mobile: req.body.mobile, name: req.body.name  })
         if (!user) {
@@ -100,17 +21,6 @@ router.post('/resendOtp', expressAsyncHandler(async (req, res, next) => {
                 message: '등록된 사용자가 없습니다.'
             })
         } else {
-            // 재전송시 기존 otp 삭제
-              const issuedOtp = await OtpNumber.find({ mobile: req.body.mobile, name: req.body.name  })
-              if(issuedOtp.length > 0) {
-                await OtpNumber.deleteMany({ mobile: req.body.mobile, name: req.body.name  })
-                console.log('기발행 otp가 삭제되었습니다, 새로운 otp를 생성합니다.')
-                generateOtp()
-              } else {
-                console.log('기발행 otp가 없습니다, 새로운 otp를 생성합니다.')
-                generateOtp()
-              }
-
         async function generateOtp() {
                 const code = Math.floor(100000 + Math.random() * 900000).toString(); 
             const otpNumber = new OtpNumber({
@@ -130,6 +40,13 @@ router.post('/resendOtp', expressAsyncHandler(async (req, res, next) => {
             }, 300000)
         }
         if (otpNumber && otpNumber.count <= 3 && user.authCount <= 9) { 
+            const issuedOtp = await OtpNumber.find({ mobile: req.body.mobile, name: req.body.name  })
+            if(issuedOtp.length > 0) {
+              await OtpNumber.deleteMany({ mobile: req.body.mobile, name: req.body.name  })
+              console.log('기발행 otp가 삭제되었습니다, 새로운 otp를 생성합니다.')
+            } else {
+              console.log('기발행 otp가 없습니다, 새로운 otp를 생성합니다.')
+            }
             const convertedPhoneNumber = `+82${req.body.mobile.slice(1)}`;
             client.messages
                 .create({
@@ -165,8 +82,7 @@ router.post('/resendOtp', expressAsyncHandler(async (req, res, next) => {
         }
         console.log('otpNumber :', otpNumber)
             }
-            
-  
+            generateOtp()
         }
     } catch (error) {
         console.log('otp 재생성 에러 :', err)
